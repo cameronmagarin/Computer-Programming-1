b1d6e6d756911a1a72545f321be7b3ae0b919dbc
cameronmagarin <122569279+cameronmagarin@users.noreply.github.com>
2023-02-05 18:35:37 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int winterCost;
+        int springCost;
+        int summerCost;
+        int fallCost; 
+        int yearlyCost;
+
+        winterCost = 250;
+        springCost = 450;
+        summerCost = 345;
+        fallCost = 125;
+        yearlyCost = winterCost + springCost + summerCost + fallCost;
+
+        System.out.println("Your winter costs were " + winterCost + " Your spring costs were" +  springCost + " Your summer costs were " + summerCost + " Your fall costs were " + fallCost);
+        System.out.println("Your yearly total was " + yearlyCost);
+    }
+}
