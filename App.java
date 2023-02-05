3ad067928b9e2cdd9d4085c82e7c5c9f98d0472a
cameronmagarin <122569279+cameronmagarin@users.noreply.github.com>
2023-02-05 18:02:51 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,50 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int intOperandA = 90;
+        int intOperandB = 78;
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intMudulo;
+
+        double doubleOperandA = 12.50;
+        double doubleOperandB = 36.25;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+        double doubleMudulo;
+
+        intSum = intOperandA + intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intMudulo = intOperandA % intOperandB;
+        System.out.println("The sum of ints of " + intOperandA + " " + intOperandB + " is" +intSum);
+        System.out.println("The product of ints of " + intOperandA + " " + intOperandB + " is " + intProduct);
+        System.out.println("The difference of ints of " + intOperandA + " " + intOperandB + " is " + intDifference);
+        System.out.println("The quotient of ints of " + intOperandA + " " +intOperandB + " is " + intQuotient);
+        System.out.println("The muduluo of ints of " + intOperandA + " " + intOperandB + " is " + intMudulo);
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleDifference = doubleOperandB - doubleOperandA;
+        doubleQuotient = doubleOperandB / doubleOperandA;
+        doubleMudulo = doubleOperandA % doubleOperandB;
+        System.out.println("The sum of doubles of " + doubleOperandA + " " + doubleOperandB + " is" + doubleSum);
+        System.out.println("The product of doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleProduct);
+        System.out.println("The difference of doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleDifference);
+        System.out.println("The quotient of doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleQuotient);
+        System.out.println("The muduluo of doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleMudulo);
+
+        int familyKids = 0;
+        boolean rainAnswer = false;
+        double gasPrice = 3.45;
+        int favNumber = 12;
+        int shoeSize = 10;
+        String birthMonth = "April";
+        String fullName = "Cameron Jacob Duldulao Magarin";
+
+    }
+}
