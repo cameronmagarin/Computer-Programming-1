8e7988c1332bacddb01c1cc73194c4c57e6b5a51
cameronmagarin <122569279+cameronmagarin@users.noreply.github.com>
2023-02-05 18:44:46 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       double cardBalance;
+       double cardInterest;
+       double firstMonth;
+       double secondMonth;
+       
+       cardBalance = 5000;
+       cardInterest = 0.17;
+       firstMonth = cardBalance + (cardBalance * cardInterest);
+       secondMonth = firstMonth + (firstMonth * cardInterest);
+       
+        System.out.println("Your initial balance was " + cardBalance + " After 1 month it will be " + firstMonth + " After 2 months it will be " +secondMonth);
+    }
+}
